9f8934b48b93a8c2de17c2c0fd3127d7e6888c12
nebori92 <nebori92@vivans.net>
2018-12-03 15:14:31 +0900

    Initial Commit

---
+++ Glue/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Glue
+//
+//  Created by 김인중 on 03/12/2018.
+//  Copyright © 2018 nebori92. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
