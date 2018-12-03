69698ef46c952c48fde3e1ae3a4b082e28a4552f
nebori92 <nebori92@vivans.net>
2018-12-03 16:57:39 +0900

    필요한 상태바 이미지 추가 및 기본 클래스 추가.

---
+++ Glue/GlueViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GlueViewController.swift
+//  Glue
+//
+//  Created by 김인중 on 03/12/2018.
+//  Copyright © 2018 nebori92. All rights reserved.
+//
+
+import Cocoa
+
+class GlueViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
