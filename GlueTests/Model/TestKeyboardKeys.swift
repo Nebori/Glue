12d629564ce697e1571a7de7fdcc0d2b1f3b5f2e
nebori92 <nebori92@vivans.net>
2018-12-07 15:32:38 +0900

    키보드와 이벤트 키 코드 파싱을 위한 KeyboardKey 구조체  및 테스트 코드 추가.

---
+++ GlueTests/Model/TestKeyboardKeys.swift
@@ -0,0 +1,52 @@
+//
+//  TestKeyboardKeys.swift
+//  GlueTests
+//
+//  Created by 김인중 on 07/12/2018.
+//  Copyright © 2018 nebori92. All rights reserved.
+//
+
+import XCTest
+@testable import Glue
+
+class TestKeyboardKeys: XCTestCase {
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testInitialKey() {
+        let expectationKeys = KeyboardKeys.q
+        let keys = KeyboardKey(.q)
+        assert(expectationKeys == keys.getKey())
+    }
+    
+    func testInitialStringKey() {
+        let expectationStr = "q"
+        let keys = KeyboardKey(.q)
+        assert(expectationStr == keys.getString())
+    }
+    
+    func testInitalUInt16Key() {
+        let expectationUInt16 = 12
+        let keys = KeyboardKey(.q)
+        assert(expectationUInt16 == keys.getUInt16())
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
