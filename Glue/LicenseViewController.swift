55bdf1f04f573c1c8a3034aa6edc0008ec7283e1
nebori92 <nebori92@vivans.net>
2019-01-08 18:15:26 +0900

    'MASShortcut' 라이선스 추가.

--- Glue/LicenseViewController.swift
+++ Glue/LicenseViewController.swift
@@ -20,7 +20,7 @@ class LicenseViewController: NSViewController, NSTextFieldDelegate {
         super.viewDidLoad()
         // Do view setup here.
         let contactString = "Contact".localized
-    
+        
         glueLicenseLabel.stringValue = "GlueLicense".localized
         contactLabel.attributedStringValue = contactString.addLink(font: NSFont.systemFont(ofSize: 14),
                                                                    wantLinkString: "nebori92@gmail.com",
@@ -36,6 +36,11 @@ class LicenseViewController: NSViewController, NSTextFieldDelegate {
         https://github.com/shadanan/mado-size
         Copyright © 2016 Shad Sharma. All rights reserved.
         MIT License
+        ----
+        MASShortcut
+        https://github.com/shpakovski/MASShortcut
+        Copyright © 2012-2013, Vadim Shpakovski. All rights reserved.
+        BSD 2-Clause License
         """
     }
     
